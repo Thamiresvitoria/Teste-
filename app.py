04cd8baca2526a134ffd0bf45387345a47c90c55
tvitoria <thamiresvitoriamunizdasilva20@gmail.com>
2025-06-24 00:31:31 -0300

    teste

---
+++ app.py
@@ -0,0 +1,35 @@
+
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,3 @@
+
+SECRET_KEY = 'minha_chave_secreta'
+DATABASE = 'site_usuarios.db'
